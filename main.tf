f6f15e0f6e146403dcc333b808aa5be93a5ec37b
itom-qe <50704200+itom-qe@users.noreply.github.com>
2020-10-28 12:51:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -12,6 +12,13 @@ resource "azurerm_storage_account" "stor" {
   account_tier             = "${var.storage_account_tier}"
   account_replication_type = "${var.storage_replication_type}"
 }
+resource "azurerm_storage_account" "stor2" {
+  name                     = "storpreftfetest2"
+  location                 = "${var.region}"
+  resource_group_name      = "${azurerm_resource_group.rg.name}"
+  account_tier             = "${var.storage_account_tier}"
+  account_replication_type = "${var.storage_replication_type}"
+}
 
 
 
